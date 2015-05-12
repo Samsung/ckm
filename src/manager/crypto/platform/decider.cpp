tizenorg/platform.core.security.key-manager

New class hierarchy (multiple backends support).
Current implemantion my use only one crypto library. The target is to
use at least two libraries at the same time (openssl and trustzone
library for arm devices).

Change-Id: I0f2105e8698543830bb9c81fad9d14f0caf0af05

src/manager/crypto/platform/decider.cpp
@@ -1,0 +1,19 @@
+#include <platform/decider.h>
+
+#include <openssl-backend/store.h>
+
+namespace CKM {
+namespace Crypto {
+
+Decider::Decider()
+  : m_store(new OpenSSL::Store)
+{}
+
+StoreShPtr Decider::getStore(const Token &) {
+    // This the place where we should choose backend bases on token information.
+    return m_store;
+};
+
+} // namespace Crypto
+} // namespace CKM
+

src/manager/crypto/platform/decider.h
@@ -1,0 +1,22 @@
+#pragma once
+
+#include <memory>
+
+#include <generic-backend/generic-store.h>
+#include <generic-backend/token.h>
+
+namespace CKM {
+namespace Crypto {
+
+class Decider {
+public:
+    Decider();
+    StoreShPtr getStore(const Token &token);
+    virtual ~Decider(){}
+private:
+    StoreShPtr m_store;
+};
+
+} // Crypto
+} // CKM
+

Samsung/ckm

Change-Id: I3563fb1c89f3603a927b8b19f6358b4fc3f5c7cf

src/manager/crypto/platform/decider.cpp
@@ -1,0 +1,19 @@
+#include <platform/decider.h>
+
+#include <sw-backend/store.h>
+
+namespace CKM {
+namespace Crypto {
+
+Decider::Decider()
+  : m_store(new SW::Store)
+{}
+
+GStoreShPtr Decider::getStore(const Token &) {
+    // This the place where we should choose backend bases on token information.
+    return m_store;
+};
+
+} // namespace Crypto
+} // namespace CKM
+

src/manager/crypto/platform/decider.h
@@ -1,0 +1,22 @@
+#pragma once
+
+#include <memory>
+
+#include <generic-backend/gstore.h>
+#include <generic-backend/token.h>
+
+namespace CKM {
+namespace Crypto {
+
+class Decider {
+public:
+    Decider();
+    GStoreShPtr getStore(const Token &token);
+    virtual ~Decider(){}
+private:
+    GStoreShPtr m_store;
+};
+
+} // Crypto
+} // CKM
+