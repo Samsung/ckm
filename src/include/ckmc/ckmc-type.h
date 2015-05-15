tizenorg/platform.core.security.key-manager

Add system database - managed by service (uid<5000) users, accessible by priviledged regular users.
Change-Id: I658b3f23503a8d73acbb86b5c10ee2339dd3984a

src/include/ckmc/ckmc-type.h
@@ -45,6 +45,15 @@ extern "C" {
 KEY_MANAGER_CAPI extern char const * const ckmc_label_name_separator;
 
 /**
+ * system database label - user may be given permission to access system
+ * database items. In such case, the alias should contain system database
+ * label.
+ * @see ckmc_label_name_separator
+ * @see key-manager_doc.h
+ */
+KEY_MANAGER_CAPI extern char const * const ckmc_label_system_database;
+
+/**
  * @brief Enumeration for key types of key manager.
  * @since_tizen 2.3
  */

src/manager/client-capi/ckmc-type.cpp
@@ -35,7 +35,8 @@
 #include <openssl/pem.h>
 
 
-const char * const ckmc_label_name_separator = CKM::LABEL_NAME_SEPARATOR;
+const char * const ckmc_label_name_separator    = CKM::LABEL_NAME_SEPARATOR;
+const char * const ckmc_label_system_database   = CKM::LABEL_SYSTEM_DB;
 
 
 int _ckmc_load_cert_from_x509(X509 *xCert, ckmc_cert_s **cert);

Samsung/ckm

Change-Id: I08b6c4718ff4219bebfd85ab942cfe22570ed0a5

src/include/ckmc/ckmc-type.h
@@ -45,6 +45,15 @@ extern "C" {
 KEY_MANAGER_CAPI extern char const * const ckmc_label_name_separator;
 
 /**
+ * shared database label - user may be given permission to access shared
+ * database items. In such case, the alias should contain shared database
+ * label.
+ * @see ckmc_label_name_separator
+ * @see key-manager_doc.h
+ */
+KEY_MANAGER_CAPI extern char const * const ckmc_label_shared_owner;
+
+/**
  * @brief Enumeration for key types of key manager.
  * @since_tizen 2.3
  */

src/manager/client-capi/ckmc-type.cpp
@@ -35,7 +35,8 @@
 #include <openssl/pem.h>
 
 
-const char * const ckmc_label_name_separator = CKM::LABEL_NAME_SEPARATOR;
+const char * const ckmc_label_name_separator    = CKM::LABEL_NAME_SEPARATOR;
+const char * const ckmc_label_shared_owner      = CKM::LABEL_SYSTEM_DB;
 
 
 int _ckmc_load_cert_from_x509(X509 *xCert, ckmc_cert_s **cert);