tizenorg/platform.core.security.key-manager

Implement asynchronous encryption/decryption API
[Feature] Encryption/decryption API implementation
[Solution] Add asynchronous interface for encryption and decryption

[Verification] Run ckm-tests --group=CKM_ENCRYPTION_DECRYPTION

Change-Id: Ie18d80a47885895aabbedc51d8bdb8ff60172726

src/include/ckm/ckm-manager-async.h
@@ -76,6 +76,8 @@ public:
 
         virtual void ReceivedSetPermission() {}
 
+        virtual void ReceivedEncryption(RawBuffer &&) {}
+
         virtual ~Observer() {}
     };
 
@@ -190,6 +192,20 @@ public:
             const Alias& alias,
             const Label& accessor,
             PermissionMask permissionMask);
+
+    void encrypt(
+            const ObserverPtr& observer,
+            const CryptoAlgorithm& algo,
+            const Alias& keyAlias,
+            const Password& password,
+            const RawBuffer& plain);
+
+    void decrypt(
+            const ObserverPtr& observer,
+            const CryptoAlgorithm& algo,
+            const Alias& keyAlias,
+            const Password& password,
+            const RawBuffer& encrypted);
 
 private:
     std::unique_ptr<Impl> m_impl;

src/manager/client-async/encryption-receiver.cpp
@@ -1,0 +1,66 @@
+/*
+ *  Copyright (c) 2000 - 2014 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
+/*
+ * @file       encryption-receiver.cpp
+ * @author     Krzysztof Jackiewicz (k.jackiewicz@samsung.com)
+ * @version    1.0
+ */
+
+#include <encryption-receiver.h>
+#include <dpl/log/log.h>
+#include <protocols.h>
+
+namespace CKM {
+
+EncryptionReceiver::EncryptionReceiver(MessageBuffer& buffer, AsyncRequest::Map& requests) :
+    m_buffer(buffer),
+    m_requests(requests)
+{
+}
+
+void EncryptionReceiver::parseResponse()
+{
+    int command = 0;
+    int id = 0;
+    int retCode;
+    RawBuffer output;
+    m_buffer.Deserialize(command, id, retCode, output);
+
+    auto it = m_requests.find(id);
+    if (it == m_requests.end()) {
+        LogError("Request with id " << id << " not found!");
+        ThrowMsg(BadResponse, "Request with id " << id << " not found!");
+    }
+
+    // let it throw
+    AsyncRequest req = std::move(m_requests.at(id));
+    m_requests.erase(id);
+
+    switch (static_cast<EncryptionCommand>(command)) {
+    case EncryptionCommand::ENCRYPT:
+    case EncryptionCommand::DECRYPT:
+        if (retCode == CKM_API_SUCCESS)
+            req.observer->ReceivedEncryption(std::move(output));
+        else
+            req.observer->ReceivedError(retCode);
+    default:
+        LogError("Unknown command id: " << command);
+        ThrowMsg(BadResponse, "Unknown command id: " << command);
+        break;
+    }
+}
+
+} /* namespace CKM */

Samsung/ckm

src/include/ckm/ckm-manager-async.h
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2000 - 2014 Samsung Electronics Co., Ltd All Rights Reserved
+ *  Copyright (c) 2000 - 2015 Samsung Electronics Co., Ltd All Rights Reserved
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -75,6 +75,9 @@ public:
         virtual void ReceivedOCSPCheck(int) {}
 
         virtual void ReceivedSetPermission() {}
+
+        virtual void ReceivedEncrypted(RawBuffer &&) {}
+        virtual void ReceivedDecrypted(RawBuffer &&) {}
 
         virtual ~Observer() {}
     };
@@ -190,6 +193,20 @@ public:
             const Alias& alias,
             const Label& accessor,
             PermissionMask permissionMask);
+
+    void encrypt(
+            const ObserverPtr& observer,
+            const CryptoAlgorithm& algo,
+            const Alias& keyAlias,
+            const Password& password,
+            const RawBuffer& plain);
+
+    void decrypt(
+            const ObserverPtr& observer,
+            const CryptoAlgorithm& algo,
+            const Alias& keyAlias,
+            const Password& password,
+            const RawBuffer& encrypted);
 
 private:
     std::unique_ptr<Impl> m_impl;

src/manager/client-async/encryption-receiver.cpp
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2000 - 2014 Samsung Electronics Co., Ltd All Rights Reserved
+ *  Copyright (c) 2000 - 2015 Samsung Electronics Co., Ltd All Rights Reserved
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -14,26 +14,30 @@
  *  limitations under the License
  */
 /*
- * @file       ocsp-receiver.cpp
+ * @file       encryption-receiver.cpp
  * @author     Krzysztof Jackiewicz (k.jackiewicz@samsung.com)
  * @version    1.0
  */
 
-#include <ocsp-receiver.h>
+#include <encryption-receiver.h>
 #include <dpl/log/log.h>
+#include <protocols.h>
 
 namespace CKM {
 
-OcspReceiver::OcspReceiver(MessageBuffer& buffer, AsyncRequest::Map& requests) :
+EncryptionReceiver::EncryptionReceiver(MessageBuffer& buffer, AsyncRequest::Map& requests) :
     m_buffer(buffer),
     m_requests(requests)
 {
 }
 
-void OcspReceiver::processResponse()
+void EncryptionReceiver::processResponse()
 {
-    int id = 0, retCode = 0, ocspStatus = 0;
-    m_buffer.Deserialize(id, retCode, ocspStatus);
+    int command = 0;
+    int id = 0;
+    int retCode;
+    RawBuffer output;
+    m_buffer.Deserialize(command, id, retCode, output);
 
     auto it = m_requests.find(id);
     if (it == m_requests.end()) {
@@ -45,10 +49,24 @@ void OcspReceiver::processResponse()
     AsyncRequest req = std::move(m_requests.at(id));
     m_requests.erase(id);
 
-    if (retCode == CKM_API_SUCCESS)
-        req.observer->ReceivedOCSPCheck(ocspStatus);
-    else
-        req.observer->ReceivedError(retCode);
+    switch (static_cast<EncryptionCommand>(command)) {
+    case EncryptionCommand::ENCRYPT:
+        if (retCode == CKM_API_SUCCESS)
+            req.observer->ReceivedEncrypted(std::move(output));
+        else
+            req.observer->ReceivedError(retCode);
+        break;
+    case EncryptionCommand::DECRYPT:
+        if (retCode == CKM_API_SUCCESS)
+            req.observer->ReceivedDecrypted(std::move(output));
+        else
+            req.observer->ReceivedError(retCode);
+        break;
+    default:
+        LogError("Unknown command id: " << command);
+        ThrowMsg(BadResponse, "Unknown command id: " << command);
+        break;
+    }
 }
 
 } /* namespace CKM */